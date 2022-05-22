5a5a608706f018c817b5994abc56936e68194e3f
sweetpea22 <cindychau24@me.com>
2022-05-22 11:05:47 -0400

    Create template

---
+++ src/pages/Eng/index.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import './index.scss';
+
+export const Eng: React.FC = () => {
+  return (
+    <div className='container'>
+      <section className='contentWrapper'>
+        <header className='headerTextWrapper'>
+          <h1 className='titleText'>Solutions & Research</h1>
+          <button className='primaryBtn'>Request a quote</button>
+        </header>
+        <div className='singleColumnText'>
+          <h4>
+            We work on some of the toughest problems in the blockchain space.
+          </h4>
+          <p>
+            We investigate hard problems and create specifications to help your protocol or team explore what’s possible. Got quandaries on technical or business frontiers? We’ll pioneer an elegant and customized solution for you. 
+          </p>
+        </div>
+        <div className='featuredContentWrapper'>
+          <img src="/eng.png" alt=""/>
+          <div className='doubleColumnWrapper'>
+            <h4>Example Network</h4>
+            <p>Read about how we helped the team at ExampleNet with writing their specification for the Alice Validator.</p>
+          </div>
+        </div>
+        <div className='techAdvisory'>
+          <div className='doubleColumnWrapper'>
+            <h4>Technical design advisory & implementation consulting</h4>
+            <div>
+              <p>We help clients solve technical problems and make better decisions.</p>
+              <p>With our domain expertise, we help existing blockchain projects and new entrants to the space build better solutions and implementations. </p>
+            </div>
+          </div>
+          <div className='itemsWrapper'>
+            <div className='item'>
+              <div></div>
+              <p className='bodyText'>
+                Cross-chain messaging
+              </p>
+            </div>
+            <div className='item'>
+              <div></div>
+              <p className='bodyText'>
+                DeFi Specifications
+              </p>
+            </div>
+            <div className='item'>
+              <div></div>
+              <p className='bodyText'>
+                Protocol Design
+              </p>
+            </div>
+            <div className='item'>
+              <div></div>
+              <p className='bodyText'>
+                Protocol Design
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='doubleColumn'>
+          <h4>Crypto-economics consulting</h4>
+          <div className='bodyTextWrapper'>
+            <p className='bodyText'>We design incentive systems that last.</p>
+            <p className='bodyText'>In crypto, solving technical challenges is often closely correlated with the economic (incentive) design of a system. This requires a particular breed of expertise—and our multi-disciplinary team has the deep, first-hand experience to tackle any crypto-economic design challenge.</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route, Outlet } from 'react-router-dom';
 import { Home } from './Home';
+import { Eng } from './Eng';
 
 export const Router: React.FC = () => (
   <>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Eng />} />
     </Routes>
   </>
 );
