7ab7bea2876fdc648d7819c9677cf8dc485130f5
sweetpea22 <cindychau24@me.com>
2022-06-06 21:40:59 -0400

    Adjust header animation

--- src/components/Header/MenuToggle.tsx
+++ src/components/Header/MenuToggle.tsx
@@ -5,26 +5,26 @@ import './index2.scss';
 export const MenuToggle = ({toggle}:any) => {
   return (
   <button className='menuToggle' onClick={toggle} >
-    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" stroke="blue" strokeWidth={2}>
-      <motion.path strokeWidth={2} stroke="#000" d="M 2 2.5 L 20 2.5"
+    <svg width="40" height="40" viewBox="0 0 30 30" fill="none" strokeWidth={2}>
+      <motion.path strokeWidth={2} stroke="#5e5e5e" d="M 2 2.5 L 30 2.5"
       variants={{
-          closed: { d: "M 2 2.5 L 20 2.5" },
-          open: { d: "M 3 16.5 L 17 2.5" }
+          closed: { d: "M 2 2.5 L 30 2.5" },
+          open: { d: "M 3 16.5 L 30 2.5" }
         }}
       />
       <motion.path
-        strokeWidth={2} stroke="#000" 
-        d="M 2 9.423 L 20 9.423"
+        strokeWidth={2} stroke="#5e5e5e" 
+        d="M 2 9.423 L 30 9.423"
         variants={{
           closed: { opacity: 1 },
           open: { opacity: 0 }
         }}
         transition={{ duration: 0.1 }}
       />
-      <motion.path strokeWidth={2} stroke="#000" d="M 2 16.346 L 20 16.346"
+      <motion.path strokeWidth={2} stroke="#5e5e5e" d="M 2 16.346 L 30 16.346"
          variants={{
-          closed: { d: "M 2 16.346 L 20 16.346" },
-          open: { d: "M 3 2.5 L 17 16.346" }
+          closed: { d: "M 2 16.346 L 30 16.346" },
+          open: { d: "M 3 2.5 L 30 16.346" }
         }}
       />
     </svg>
